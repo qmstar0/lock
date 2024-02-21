cf3ab721ffd49b1420181b5cee6110c07382b28b
QMStar <qmstar0@qq.com>
2024-02-21 23:16:48 +0800

    v0.1.0

--- closer.go
+++ closer.go
@@ -2,15 +2,40 @@ package lock
 
 import "sync"
 
-type Closer struct {
-	Closed  bool
+type Closer interface {
+	Closed() bool
+	Closing() <-chan struct{}
+	LockedRun(fn func() error) error
+	Reset()
+}
+type closer struct {
+	closed  bool
 	closing chan struct{}
-	sync.Mutex
+	lock    sync.Mutex
 }
 
-func (c *Closer) Closing() <-chan struct{} {
-	if c.closing == nil {
-		c.closing = make(chan struct{})
+func NewCloser() Closer {
+	return &closer{
+		closed:  false,
+		closing: make(chan struct{}),
+		lock:    sync.Mutex{},
 	}
+}
+
+func (c *closer) Closed() bool {
+	return c.closed
+}
+
+func (c *closer) Closing() <-chan struct{} {
 	return c.closing
+}
+func (c *closer) LockedRun(fn func() error) error {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return fn()
+}
+
+func (c *closer) Reset() {
+	c.closed = false
+	c.closing = make(chan struct{})
 }
